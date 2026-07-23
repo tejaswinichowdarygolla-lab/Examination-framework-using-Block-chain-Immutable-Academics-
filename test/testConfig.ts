decc1ad43ab7b2ad1ffdc38ef05aacbe045b42d2
tejaswinichowdarygolla-lab <tejaswinichowdarygolla@gmail.com>
2026-07-23 17:51:56 +0530

    Add files via upload

---
+++ test/testConfig.ts
@@ -0,0 +1,13 @@
+export const TEST_MODE_ACTIVE: number = 1; // Set to 1 to enable test environment
+
+export const TEST_CREDENTIALS = {
+  admin: "0x76Cb9ceB5ae1E2f14c6781053D3896527d26FA14",
+  teacher: "0x2e95aDda11F2dD232472E1788B2F24f5C72e197b",
+  students: [
+    
+   
+  ]
+};
+
+// Plug and Unplug Mechanism for MetaMask Authentication
+export const METAMASK_FLAG: number = 0; // 0 = Remove authentication, 1 = Keep as is
